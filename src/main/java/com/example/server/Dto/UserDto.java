1df9665a6eab7847bdd579154fe184926191ca86
kgt-it <khushisaraswat69@gmail.com>
2025-05-01 06:00:33 -0700

    java server

---
+++ src/main/java/com/example/server/Dto/UserDto.java
@@ -0,0 +1,14 @@
+package com.example.server.Dto;
+
+
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    
+    private String username;
+    private String password;
+     private String role;
+   
+}
